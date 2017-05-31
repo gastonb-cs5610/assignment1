0b691b62de559bbf1a967d580f013896921aa2e4
Blue Gaston <bluegaston@Marys-MacBook-Pro.local>
2017-05-31 16:30:45 -0700

    fixing

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-var ipaddress = '127.0.0.1';
-var port      =  process.env.PORT || 3000;
+app.set('port', (process.env.PORT || 3000));
 
+var ipaddress = '127.0.0.1';
 
 app.use(express.static(__dirname+'/public/UserPages'));
-app.listen(port, ipaddress);
 
 
 
-console.log("hello world!");
+app.get('/', function(request, response) {
+    var result = 'App is running';
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
