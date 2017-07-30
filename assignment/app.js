a973b14502d6556d8729302e3f0a57cb0a87ad40
Blue Gaston <bluegaston@Marys-MacBook-Pro.local>
2017-07-30 09:51:52 -0700

    done

--- assignment/app.js
+++ assignment/app.js
@@ -2,8 +2,19 @@ module.exports = function(){
 
 
     var mongoose = require('mongoose');
-    //mongoose.connect('mongodb://localhost/webdev');
-    //mongoose.Promise = require('q').Promise;
+    var connectionString =  null;
+
+    if (process.env.MONGODB_URI) {
+        connectionString = 'mongodb://bbluegaston:ass1gnment@ds055495.mlab.com:55495/bgwebdev';
+    }
+    else
+    {
+        connectionString = connectionString = 'mongodb://localhost:27017/cs5610'
+    }
+
+    mongoose.connect(connectionString, {useMongoClient:true});
+
+
 
     require("./services/user.service.server.js");
     require("./services/website.service.server.js");
